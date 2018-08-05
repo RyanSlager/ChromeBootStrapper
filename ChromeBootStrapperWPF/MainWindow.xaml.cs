2adc825c7fe54544a2a5f2bf71663259004dbdfa
RyanSlager <fryanslaucerers@gmail.com>
2018-08-05 00:45:22 -0400

    Manifest library, Model, Button functionality added. TODO: Bindings

--- ChromeBootStrapperWPF/MainWindow.xaml.cs
+++ ChromeBootStrapperWPF/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
+using Newtonsoft.Json;
+using ChromeBootStrapper;
+using System.Windows.Forms;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace ChromeBootStrapperWPF
 {
@@ -20,9 +12,72 @@ namespace ChromeBootStrapperWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Model m = new Model();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = m;
+        }
+
+        private void launchBrowser(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button b = (System.Windows.Controls.Button)sender;
+            
+            //System.Windows.MessageBox.Show(name);
+
+            if(b.Name.Equals("iconSearch"))
+            {
+                m.IconText = pickIcon();
+            }
+            else if(b.Name.Equals("locSearch"))
+            {
+                m.LocationText = pickLocation();
+            }
+
+            
+            System.Windows.MessageBox.Show(m.IconText, m.LocationText);
+        }
+
+        private void createExtension(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show($"{m.IconText}, {m.LocationText}, {m.NameText}, {m.DescriptionText}, {m.AuthorText}");
+
+            //Manifest man = new Manifest(m.LocationText, m.NameText, m.DescriptionText,
+            //    m.AuthorText, false);
+
+            //man.writeManifest();
+        }
+
+        private string pickIcon()
+        {
+            OpenFileDialog iconFBD = new OpenFileDialog();
+            string iconPath = "icon.png";
+
+            if (iconFBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                iconPath = iconFBD.FileName;
+                return iconPath;
+            }
+            return iconPath;
+        }
+
+        private string pickLocation()
+        {
+            FolderBrowserDialog storageDirFBD = new FolderBrowserDialog();
+            string dirPath;
+
+            if (storageDirFBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dirPath = storageDirFBD.SelectedPath;
+                return dirPath;
+            }
+            else
+            {
+                dirPath = dirPath = $"%USERPROFILE%\\Desktop\\Extension";
+                DirectoryInfo dir = Directory.CreateDirectory(dirPath);
+                return dirPath;
+            }
         }
     }
 }
---
+++ ChromeBootStrapperWPF/Model.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeBootStrapperWPF
+{
+    class Model
+    {
+        private string name;
+
+        public string NameText
+        {
+            get { return name;  }
+            set
+            {
+                name = value;
+                OnPropertyChange("NameText");
+            }
+        }
+
+        private string description;
+
+        public string DescriptionText
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChange("DescriptionText");
+            }
+        }
+
+        private string author;
+
+        public string AuthorText
+        {
+            get { return author; }
+            set
+            {
+                author = value;
+                OnPropertyChange("AuthorText");
+            }
+        }
+
+        private string location;
+
+        public string LocationText
+        {
+            get { return location; }
+            set
+            {
+                location = value;
+                OnPropertyChange("LocationText");
+            }
+        }
+
+        private string icon;
+
+        public string IconText
+        {
+            get { return icon; }
+            set
+            {
+                icon = value;
+                OnPropertyChange("IconText");
+            }
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChange(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEvent = PropertyChanged;
+            if (propertyChangedEvent != null)
+            {
+                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
